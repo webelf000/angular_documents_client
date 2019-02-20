daf590504febac9632506af51b4ee442791a5487
webelf000 <steven.jung86000@gmail.com>
2019-02-20 18:19:27 +0100

    documents clean up from old components

--- src/lib/documents.module.ts
+++ src/lib/documents.module.ts
@@ -3,7 +3,6 @@ import { DocumentMainComponent } from './pages/documents-main/document-main.comp
 import { DocumentsRoutingModule } from '@libs/documents/src/lib/documents-routing.module';
 import { MatDialogModule, MatSnackBarModule } from '@angular/material';
 import { MidgardModule } from '@libs/midgard-angular/src/lib/midgard.module';
-import { TopBarModule } from '@libs/midgard-angular/src/lib/components/top-bar-actions/top-bar.module';
 import { MgSpinnerModule } from '@libs/midgard-angular/src/lib/components/spinner/spinner.module';
 import { PictureListItemComponent } from '@libs/documents/src/lib/components/picture-list/picture-list-item/picture-list-item.component';
 import { DocumentFormComponent } from '@libs/documents/src/lib/components/document-form/document-form.component';
@@ -15,23 +14,21 @@ import { DocumentPreviewComponent } from '@libs/documents/src/lib/components/doc
 import { DocumentPreuploadComponent } from '@libs/documents/src/lib/components/document-preupload/document-preupload.component';
 import { BackgroundImageDirective } from '@libs/documents/src/lib/directives/background-image.directive';
 import { CommonModule } from '@angular/common';
-import { MgDragAndDropModule } from '@libs/midgard-angular/src/lib/components/drag-and-drop/drag-and-drop.module';
-import { LoaderModule } from '@libs/midgard-angular/src/lib/components/loader/loader.module';
-import { CardModule } from '@libs/midgard-angular/src/lib/components/card/card.module';
-import { GridFormsModule } from '@libs/midgard-angular/src/lib/components/form-elements/grid-forms/grid-forms.module';
 import { FileExtensionHelper } from '@libs/documents/src/lib/helpers/file-extension.helper';
 import { FileSavingService } from '@libs/documents/src/lib/services/file-saving.service';
 import { ImageLoadingService } from '@libs/documents/src/lib/services/image-loading.service';
 import { MidgardSharedTranslationModule } from '@libs/midgard-angular/src/lib/modules/translation/translation.shared.module';
 import { DeleteConfirmationModule } from '@libs/midgard-angular/src/lib/components/delete-confirmation/delete-confirmation.module';
 import { DeleteConfirmationComponent } from '@libs/midgard-angular/src/lib/components/delete-confirmation/delete-confirmation.component';
-import {FjButtonModule} from 'freyja-ui';
-import {FjModalModule} from 'freyja-ui';
-import {FjTextInputModule} from 'freyja-ui';
-import {FjDatePickerModule} from 'freyja-ui';
-import {FjNativeDropdownModule} from 'freyja-ui';
-import {FjDragAndDropModule} from 'freyja-ui';
-import {IconModule} from 'freyja-ui';
+import {
+  FjButtonModule,
+  FjModalModule,
+  FjTextInputModule,
+  FjDatePickerModule,
+  FjNativeDropdownModule,
+  FjDragAndDropModule,
+  IconModule
+} from 'freyja-ui';
 import {MidgardCrudModule} from '../../../midgard-angular/src/lib/modules/crud/crud.module';
 
 @NgModule({
@@ -42,15 +39,10 @@ import {MidgardCrudModule} from '../../../midgard-angular/src/lib/modules/crud/c
     MidgardModule,
     MatSnackBarModule,
     MatDialogModule,
-    TopBarModule,
     MgSpinnerModule,
     DeleteConfirmationModule,
     TableModule,
-    MgDragAndDropModule,
-    LoaderModule,
     IconModule,
-    CardModule,
-    GridFormsModule,
     FjButtonModule,
     FjModalModule,
     FjTextInputModule,
